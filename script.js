4d152813d953423fb22d096f47579764604aa4e3
Greg Duckworth <gregory.duckworth@nearform.com>
2023-02-03 12:07:27 +0000

    commenting

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 module.exports = async () => {
   const {ACTION_NAME} = process.env
   const [repoOrg, repoName] = ACTION_NAME.split('_')
-  console.log(repoOrg)
-  console.log(repoName)
+  console.log("-----------------")
+  console.log(`repoOrg: ${repoOrg}`)
+  console.log(`repoName: ${repoName}`)
+  console.log("-----------------")
 }
