82a719cb577156e43642ea53cf7490d1df966a19
Greg Duckworth <gregory.duckworth@nearform.com>
2023-02-03 12:05:21 +0000

    core

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 module.exports = async ({core}) => {
   const {ACTION_NAME} = process.env
   const [repoOrg, repoName] = ACTION_NAME.split('_')
-  console.log(repoOrg)
-  console.log(repoName)
+  core.log(repoOrg)
+  core.log(repoName)
 }
